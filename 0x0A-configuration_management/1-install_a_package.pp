610c26a1bace62bcc3340ce2139ad2c29b4cc963
iku96 <ikumwana@gmail.com>
2024-07-30 06:48:18 -0700

    Think I finally got it

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,9 +5,9 @@ package {'python3-pip':
   ensure => 'installed'
 }
 
-# Install flask version 2.1.0 using pip3
-exec {'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+# Install flask version 2.1.0 and werkzeug ver 2.1.1 using pip3
+exec {'install_flask_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1',
   path    => ['/usr/bin', '/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0" && /usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
