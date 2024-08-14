07cf2ed8e08d161863fe20ca82041a88193e6c08
Iku96 <ikumwana@gmail.com>
2024-08-13 19:55:37 -0700

    Solved Task 2 and 3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,21 @@
+#!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    # Define the URL for the API request
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    # Set a custom User-Agent
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    # Make the API request
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    # Check if the response is valid
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-        # Extract and print the titles of the first 10 hot posts
-        posts = data.get("data", {}).get("children", [])
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-    else:
-        # Print None for an invalid subreddit
-        print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
