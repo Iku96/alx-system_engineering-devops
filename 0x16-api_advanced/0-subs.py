243fbe86d7bb92f2de8880cdf488e91fce68109e
Iku96 <40354350+Iku96@users.noreply.github.com>
2024-08-14 05:32:31 +0300

    Solved Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script that queries the Reddit API to find the number
+of subscribers for a given subreddit"""
+import requests
+
+def number_of_subscribers(subreddit):
+    # Defining the URL for the API request
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    # Setting a customer user-agent to avoid request blocking
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    # Making the API request
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Checking if the response is valid
+    if response.status_code == 200:
+        # Parsing the JSON response
+        data = response.json()
+        # Extracting the number of subscribers
+        return data.get("data", {}).get("subscribers", 0)
+    else:
+        # Returning 0 for an invalid subreddit
+        return 0
